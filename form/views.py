3ab3b722a404c4ac490ce3227a76211a190f7489
denisovlev <denisovlev@ya.ru>
2018-04-10 13:59:04 +0200

    lab6 proper chart rendering

--- form/views.py
+++ form/views.py
@@ -1,3 +1,7 @@
+import json
+import os
+from collections import Counter
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Leads
@@ -25,3 +29,19 @@ def search(request):
         items = leads.get_lead_domains()
         count = [(item["domain"], item["num"]) for item in items]
         return render(request, 'search.html', {'domains': sorted(count)})
+
+import vincent
+from django.conf import settings
+
+BASE_DIR = getattr(settings, "BASE_DIR", None)
+
+
+def chart(request):
+    leads = Leads()
+    items = leads.get_lead_domains()
+    freq = [int(item["num"]) for item in items]
+    labels = [item["domain"] for item in items]
+    data = {'data': freq, 'x': labels}
+    bar = vincent.Bar(data, iter_idx='x')
+    content = json.dumps(bar.to_json())
+    return render(request, 'chart.html', {'items': items, 'content': content})
